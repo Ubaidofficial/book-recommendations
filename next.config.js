ce2b08157d8c6590fa7004ab5c37b6e65fd21874
Your Name <imubaid93@gmail.com>
2026-06-02 13:26:15 +0200

    Redirect merged people aliases

--- next.config.js
+++ next.config.js
@@ -1,5 +1,45 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/people/gates",
+        destination: "/people/bill-gates",
+        permanent: true,
+      },
+      {
+        source: "/people/ravikant",
+        destination: "/people/naval-ravikant",
+        permanent: true,
+      },
+      {
+        source: "/people/andreessen",
+        destination: "/people/marc-andreessen",
+        permanent: true,
+      },
+      {
+        source: "/people/obama",
+        destination: "/people/barack-obama",
+        permanent: true,
+      },
+      {
+        source: "/people/ferriss",
+        destination: "/people/tim-ferriss",
+        permanent: true,
+      },
+      {
+        source: "/people/collison",
+        destination: "/people/patrick-collison",
+        permanent: true,
+      },
+      {
+        source: "/people/peterson",
+        destination: "/people/jordan-peterson",
+        permanent: true,
+      },
+    ];
+  },
+
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
